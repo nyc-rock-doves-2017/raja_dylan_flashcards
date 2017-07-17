9867b63d2416e4b18bde6451b208c38aef50e4db
Dylan Yang & Raja <pair+dylanlovescoffee.raja-sharma@devbootcamp.com>
2017-07-17 12:26:09 -0400

    Add seeds. Add BCrypt.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:'me', email:'me@me.com', password:'password')
+
+deck = Deck.create(name:'Study math')
+
+30.times do
+  Card.create(deck: deck, question: Faker::GameOfThrones.character, answer: 'answer')
+end
